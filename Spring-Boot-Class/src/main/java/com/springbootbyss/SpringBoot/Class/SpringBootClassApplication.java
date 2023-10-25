c53925dc620e1ea5608ec76c7c38b2f5c7324650
Santosh Shah <santoshofficial679@gmail.com>
2023-10-25 05:50:37 +0545

    Spring boot final revision started

---
+++ Spring-Boot-Class/src/main/java/com/springbootbyss/SpringBoot/Class/SpringBootClassApplication.java
@@ -0,0 +1,14 @@
+package com.springbootbyss.SpringBoot.Class;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootClassApplication.class, args);
+		System.out.println("My name is Santosh Shah");
+	}
+
+}
---
+++ Spring-Boot-Class/src/test/java/com/springbootbyss/SpringBoot/Class/SpringBootClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootbyss.SpringBoot.Class;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
